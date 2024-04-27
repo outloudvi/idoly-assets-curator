0a5ccf13e564b34dcf677c09b93adcdede124d14
Outvi V <git@outv.im>
2024-04-27 21:18:18 +0800

    feat(vercel): add server-side cache for positive results

--- vercel/img.py
+++ vercel/img.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse
 
 from agents.img import ImageAgent
+from utils import POSITIVE_CACHE_TIME, NEGATIVE_CACHE_TIME, POSITIVE_ALT_CACHE_TIME
 
 
 class handler(BaseHTTPRequestHandler):
@@ -12,6 +13,8 @@ class handler(BaseHTTPRequestHandler):
         if len(path) != 4:
             self.send_response(400)
             self.send_header('Content-type', 'text/plain')
+            self.send_header(
+                'Cache-Control', f's-maxage={NEGATIVE_CACHE_TIME}')
             self.end_headers()
             message = f"Invalid id in path: {path}"
             self.wfile.write(message.encode())
@@ -24,11 +27,15 @@ class handler(BaseHTTPRequestHandler):
             self.send_header('Location', result[1])
             self.send_header('Access-Control-Allow-Origin', '*')
             self.send_header('Access-Control-Allow-Methods', 'GET')
+            self.send_header(
+                'Cache-Control', f's-maxage={POSITIVE_CACHE_TIME}, stale-while-revalidate={POSITIVE_ALT_CACHE_TIME}')
             self.end_headers()
             return
         else:
             self.send_response(result[0])
             self.send_header('Content-type', 'text/plain')
+            self.send_header(
+                'Cache-Control', f's-maxage={NEGATIVE_CACHE_TIME}')
             self.end_headers()
             self.wfile.write(result[1].encode())
             return
