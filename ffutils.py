4775404145aaac429ff46b6cb11bddb47aeb3bf4
Outvi V <git@outv.im>
2024-05-23 09:37:23 +0800

    chore(ffutils): don't display stderr unless error

--- ffutils.py
+++ ffutils.py
@@ -7,12 +7,14 @@ ffmpeg_path = os.path.join(dir_path, "ffmpeg")
 
 
 def ffmpeg_convert_format(byt, infmt, outfmt):
-    return (
-        ffmpeg
-        .input("pipe:", format=infmt)
-        .output("pipe:", format=outfmt)
-        .run(cmd=ffmpeg_path, input=byt, capture_stdout=True)
-    )[0]
+    try:
+        (out, err) = ffmpeg.input("pipe:", format=infmt).input("pipe:", format=infmt).output(
+            "pipe:", format=outfmt).run(cmd=ffmpeg_path, input=byt, capture_stdout=True)
+        return out
+    except Exception as e:
+        print(f"Exception when calling ffmpeg: {e}")
+        print(e.stderr)
+        raise e
 
 
 def wav_to_opus(byt):
